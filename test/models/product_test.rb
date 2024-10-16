4d99f55d0b9b72e47ad7ff3fe4595f3bf1208167
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2024-10-16 14:32:54 +0200

    controllers tests change

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -44,6 +44,6 @@ class ProductTest < ActiveSupport::TestCase
   test 'product is not valid without a unique title' do
     product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'fred.gif')
     product.invalid?
-    assert_equal ['has already been taken'], product.errors[:title]
+    assert_equal ['Title should be unique'], product.errors[:title]
   end
 end
